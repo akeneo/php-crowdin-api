79b411618ba20a0011d2f20c6c7299749d0ccb7b
Laurent <laurent@ghirardotti.fr>
2016-04-29 16:17:21 +0800

    Static scalar expressions definition does not work in PHP 5.5

--- spec/Akeneo/Crowdin/FileReaderSpec.php
+++ spec/Akeneo/Crowdin/FileReaderSpec.php
@@ -8,18 +8,18 @@ use Prophecy\Argument;
 
 class FileReaderSpec extends ObjectBehavior
 {
-    const EXISTING_FILE_PATH = __DIR__ . '/../../fixtures/messages.en.yml';
-    const NOT_EXISTING_FILE_PATH = __DIR__ . '/../../fixtures/not_existing.yml';
+    const EXISTING_FILE_PATH = '/../../fixtures/messages.en.yml';
+    const NOT_EXISTING_FILE_PATH = '/../../fixtures/not_existing.yml';
 
     public function it_reads_a_stream(Translation $translation)
     {
-        $translation->getLocalPath()->willReturn(self::EXISTING_FILE_PATH);
+        $translation->getLocalPath()->willReturn(__DIR__ . self::EXISTING_FILE_PATH);
         $this->readTranslation($translation)->shouldBeResource();
     }
 
     public function it_throws_an_exception_when_file_cannot_be_found(Translation $translation)
     {
-        $translation->getLocalPath()->willReturn(self::NOT_EXISTING_FILE_PATH);
+        $translation->getLocalPath()->willReturn(__DIR__ . self::NOT_EXISTING_FILE_PATH);
         $this
             ->shouldThrow('Akeneo\Crowdin\FileNotFoundException')
             ->during('readTranslation', [$translation]);
